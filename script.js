46162402965449c655ce8175551a93b8062f9cf1
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2021-06-01 01:39:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var grid = [], colors = [];
-colors[0] = "#962d2d"; colors[1] = "#f6f5f5";
-
+var grid = [], colors = [], idLen = 0;
+colors[0] = "#962d2d"; colors[1] = "#a7bbc7";
+var beginSub = new Map();
+var subMatrix = new Map();
+ 
 window.addEventListener('DOMContentLoaded', function() {
         var gameBoardClass = document.querySelector(".gameBoard");
         for(var i = 0; i < 9; ++i) {
                 grid[i] = [];
                 for(var j = 0; j < 9; ++j) {
-                    $(gameBoardClass).append(`<input type = "text" class = "boardBtn" id = "`+ i + " " + j + `" oninput = "autoCorrection(id);" />`);
+                    $(gameBoardClass).append(`<input type = "text" class = "boardBtn" id = "`+ i + " " + j + `" oninput = "autoCorrection(id);"  maxlength = 1/>`);
                      grid[i][j] = 0;
             }
             $(gameBoardClass).append(`<br>`);
         }
-          
+        setSubGrids();
+        insert();
 });
 
 
@@ -21,7 +24,7 @@ function autoCorrection(id) {
         var pos = (verifier(id) == 1) ? 1 : 0;
       
         return document.getElementById(id).style.background = colors[pos];
-  
+       
 }
 
 function verifier(id) {
@@ -29,7 +32,7 @@ function verifier(id) {
         var row = parseInt(id.charAt(0)), col = parseInt(id.charAt(2));
         
         grid[row][col] = value;
-        if(checkRowCol(row, col) == 1 && checkBox(row, col) == 1)
+        if((checkRowCol(row, col) == 1 && checkBox(row, col) == 1)|| grid[row][col] == 0)
                 return 1;
         
         grid[row][col] = 0;
@@ -37,15 +40,17 @@ function verifier(id) {
 }
 
 function checkRowCol(row, col) {
-        var check = 0;
         for(var i = 0; i < 9; ++i)
                 if(grid[row][i] == grid[row][col] && col != i) {
                         return 0;
+                        
                 }
+
           
         for(var i = 0; i < 9; ++i)
                 if(grid[i][col] == grid[row][col] && row != i) {
                         return 0;
+                       
                 }
      
         return 1;
@@ -53,25 +58,60 @@ function checkRowCol(row, col) {
 }
 
 function checkBox(row, col) {   
-        var startRow = 0, startCol = 0;
-        if(row >= 2) {
-                startRow = row - 2;
-        } else if(row == 1) {
-                startRow = row - 1;
+        var ids = (row + " " + col.toString()), beginID;
+        // I take the beginning of the submatrix of which this element is part
+        for(var i = 1; i <= 9; ++i) {
+                if(subMatrix[ids] == i) {
+                        beginID = beginSub[i];
+                        break;
+                }
         }
-          
-        if(col >= 2) {
-                startCol = col - 2;
-        } else if(col == 1) {
-                startCol = col - 1;
-        }
-
-        for(var i = startRow; i <= startRow + 2; ++i) {
-                for(var j = startCol; j <= startCol + 2; ++j)
-                       if(grid[i][j] == grid[row][col] && (row != i && col != j)) {
-                                        return 0;
-                       }                             
+        
+        var r = parseInt(beginID[0]), c = parseInt(beginID[2]);
+        for(var i = r; i < r + 2; ++i) {
+                for(var j = c; j < c + 2; ++j) 
+                        if(grid[i][j] == grid[row][col] && (i != row && j != col))
+                                return 0;
         }
 
-        return 1;
+        return 1;      
+}
+
+function setSubGrids() {
+                var bln = 0, fln = 3, gridcolor = [], pos, bol = false, beg = 1;
+                gridcolor[0] = "#232323"; gridcolor[1] = "#91091e";            
+                for(var i = fln; i <= 9; i += 3, bln += 3) {
+                        var fcol = 3, bcol = 0;
+                        for(var j = fcol; j <= 9; j += 3, bcol += 3) {
+                                pos = (bol == true) ? 1 : 0;
+                                beginSub[beg] = (bln + " " + bcol.toString());
+
+                                for(var n = bln; n < i; ++n) {
+                                        for(var m = bcol; m < j; ++m) {
+                                                var ids = (n + " " + m.toString());
+                                                document.getElementById(ids).style.borderColor = gridcolor[pos];
+                                                subMatrix[ids] = beg;
+                                        }
+                                }
+                                bol = !bol;
+                                ++beg;
+                        }                
+                }
+
+}
+
+function insert() {
+        var impoort = [0,0,4,0,6,0,0,0,0,0,0,0,9,4,0,6,0,0,0,6,3,7,0,0,5,0,4,9,0,0,0,3,6,4,0,7,0,3,0,8,2,0,0,0,6,0,0,6,4,0,0,3,0,0,6,1,0,3,0,0,0,0,0,3,0,5,0,0,4,8,0,0,0,4,8,0,0,0,0,0,0];
+        var n = 0;
+        for(var i = 0; i < 9; ++i) {
+                for(var j = 0; j < 9; ++j, ++n) {
+                        var ids = (i + " " + j.toString());
+                        if(impoort[n] != 0) {
+                                grid[i][j] = impoort[n];
+                                document.getElementById(ids).value = impoort[n];
+                                document.getElementById(ids).disabled = true;
+                        }
+                }
+
+        } 
 }
