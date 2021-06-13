abd80c14ea0ad98b3b26f6d9fcf638204069c1a7
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2021-06-13 15:40:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,111 +1,104 @@
-var grid = [];
- 
-window.addEventListener('DOMContentLoaded', function() {
-        var gameBoardClass = document.querySelector(".gameBoard");
-        for(var i = 0; i < 9; ++i) {
-                grid[i] = [];
-                for(var j = 0; j < 9; ++j) {
-                    $(gameBoardClass).append(`<input type = "text" class = "boardBtn" id = "`+ i + " " + j + `" oninput = "verifier(id);"  maxlength = 1/>`);
-                     grid[i][j] = 0;
-            }
-            $(gameBoardClass).append(`<br>`);
-        }
-        $(gameBoardClass).append(`<br>`);
-        $(gameBoardClass).append(`<button class = "restart" onclick = "location.reload();">New Board</button>`);
+let grid = [];
+let subGrid = ["#293b5f", "#fb9300"];
 
-        setSubGrids();
-        randomInsert();
+window.addEventListener('DOMContentLoaded', function() {
+                const gameBoardClass = document.querySelector(".gameBoard");
+
+                for(let row = 0; row < 9; ++row) {
+                        grid[row] = [];
+                        for(let col = 0; col < 9; ++col) {
+                                let pos = (col >= 3 && col < 6) || (row >= 3 && row < 6) ? 1 : 0;
+                                $(gameBoardClass).append(`<input class = "boardBtn" id = "`+ row + " " + col + `" oninput = "verifier(id);" maxlength = 1/>`);
+                                document.getElementById(row + " " + col).style.border = "1px solid" + subGrid[pos];
+                        }
+                        
+                
+                        $(gameBoardClass).append(`<br>`);
+                }
+                randomInsert();
+                $(gameBoardClass).append(`<br>`);
+                $(gameBoardClass).append(`<button class = "restart" onclick = "location.reload();">New Board</button>`);
+
 });
 
-function verifier(id) {        
-        let value = document.getElementById(id).value;
-        let row = parseInt(id.charAt(0)), col = parseInt(id.charAt(2));
-        
-        grid[row][col] = parseInt(value);
-        if((checkRowCol(row, col) == 1 && checkBox(row, col) == 1))
-                return document.getElementById(id).style.background = "#f8eded";
-        
-        grid[row][col] = 0;
-        return document.getElementById(id).style.background = "#962d2d";
-}
-
-function checkRowCol(row, col) {
-        for(let i = 0; i < 9; ++i)
-                if(grid[row][i] === grid[row][col] && col != i) {
-                        return 0;
-                        
-                }
-
-          
-        for(let i = 0; i < 9; ++i)
-                if(grid[i][col] === grid[row][col] && row != i) {
-                        return 0;
-                       
-                }
-     
-        return 1;   
-}
-
-function checkBox(row, col) {   
-       let beginRow = row - (row % 3);
-       let beginCol = col - (col % 3);
-
-       beginRow += (beginRow % 3 != 0) ? 1 : 0;
-       beginCol += (beginCol % 3 != 0) ? 1 : 0;  
-
-       for(let i = beginRow; i <= beginRow + 2; ++i) {
-                for(let j = beginCol; j <= beginCol + 2; ++j) {
-                        
-                        if(grid[i][j] === grid[row][col] && (i !== row && j !== col)) {
-                                
-                                return 0;
-                        }
-                }
-        }
-
-        return 1;      
-}
-
-function setSubGrids() {
-                let bln = 0, fln = 3, gridcolor = [], pos, bol = false, beg = 1;
-                gridcolor[0] = "#232323"; gridcolor[1] = "#185adb";            
-                for(let i = fln; i <= 9; i += 3, bln += 3) {
-                        let fcol = 3, bcol = 0;
-                        for(let j = fcol; j <= 9; j += 3, bcol += 3) {
-                                pos = (bol == true) ? 1 : 0;
-                                
-                                for(let n = bln; n < i; ++n) {
-                                        for(let m = bcol; m < j; ++m) {
-                                                let ids = (n + " " + m.toString());
-                                                document.getElementById(ids).style.borderColor = gridcolor[pos];
-                                        }
-                                }
-                                bol = !bol;
-                                ++beg;
-                        }                
-                }
-
-}
 
 function randomInsert() {               
                 for(let row = 0; row < 9; ++row) {
-                        let subCount = 0;                                             
-                        for(let i = 0; i < 3; ++i) {
-                                var col = Math.floor(Math.random() * 3) + subCount;
-                                var val = (Math.floor(Math.random() * 9) + 1) % 10;
-                                var ids = (row + " " + col.toString());
-                                grid[row][col] = val;
-                                
-                                if(checkRowCol(row, col) == 1 && checkBox(row, col) == 1) {
+                        let subCount = 0, trys = 4;
+                        while(--trys) {
+                                const number = {
+                                        col: randomizer(subCount, 3),
+                                        value: randomizer(1, 8) % 10,
+                                }
+                                let ids = (row + " " + number.col.toString());
+                                grid[row][number.col] = number.value;
                                         
-                                        document.getElementById(ids).value = grid[row][col];
+                                if(checkRowCol(row, number.col) == 1 && checkBox(row, number.col) == 1) {
+                                                
+                                        document.getElementById(ids).value = grid[row][number.col];
                                         document.getElementById(ids).disabled = true;
-                                        
+                                                
                                 } else {
                                         document.getElementById(ids).value = " ";
-                                        grid[row][col] = 0;
+                                                grid[row][number.col] = 0;
                                 }
-                                subCount+=3;                         
+                                subCount += 3;
+                                
                         }                   
                 }
+}
+
+
+function verifier(id) {        
+                let value = document.getElementById(id).value;
+                let row = parseInt(id.charAt(0)), col = parseInt(id.charAt(2));
+                
+                grid[row][col] = parseInt(value);
+                console.log(row, col, grid[row][col]);
+                if((checkRowCol(row, col) == 1 && checkBox(row, col) == 1))
+                        return document.getElementById(id).style.background = "#f8eded";
+                
+                grid[row][col] = 0;
+                return document.getElementById(id).style.background = "#962d2d";
+}
+
+function checkRowCol(row, col) {
+                for(let i = 0; i < 9; ++i)
+                        if(grid[row][i] === grid[row][col] && col != i) {
+                                return 0;
+                                
+                        }
+
+                
+                for(let i = 0; i < 9; ++i)
+                        if(grid[i][col] === grid[row][col] && row != i) {
+                                return 0;
+                        
+                        }
+        
+                return 1;   
+}
+
+function checkBox(row, col) {   
+                let beginRow = row - (row % 3);
+                let beginCol = col - (col % 3);
+
+                beginRow += (beginRow % 3 != 0) ? 1 : 0;
+                beginCol += (beginCol % 3 != 0) ? 1 : 0;  
+
+                for(let i = beginRow; i <= beginRow + 2; ++i) {
+                                for(let j = beginCol; j <= beginCol + 2; ++j) {
+                                        
+                                        if(grid[i][j] === grid[row][col] && (i !== row && j !== col)) {
+                                                
+                                                return 0;
+                                        }
+                                }
+                        }
+
+                        return 1;      
+}
+
+function randomizer(item1, item2) {
+                return item1 + Math.floor(Math.random() * item2);
 }
